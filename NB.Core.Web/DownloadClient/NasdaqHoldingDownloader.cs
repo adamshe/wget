4ee99ea224845255605715bfe7816903847a744a
adamshe <adamshe@yahoo.com>
2015-03-13 22:37:49 -0400

    test

--- NB.Core.Web/DownloadClient/NasdaqHoldingDownloader.cs
+++ NB.Core.Web/DownloadClient/NasdaqHoldingDownloader.cs
@@ -27,20 +27,37 @@ namespace NB.Core.Web.DownloadClient
                 /*due to attribute without quotes, can't be parsed it as xml*/
                 string startPattern = "<form";
                 string endPattern = "</form>";
+                string startTablePattern = "<table>";
+                string endTablePattern = "</table>";
+
                 int startIndex = normalizeContent.IndexOfOccurence(startPattern, 4);
                 int endIndex = normalizeContent.IndexOf(endPattern, startIndex);
                 normalizeContent = normalizeContent.Substring(startIndex, endIndex - startIndex + endPattern.Length);
                 var tempnormalizeContent = normalizeContent.Replace("\r\n", " ").Replace("\t", " ").Replace("\n", " ");
                 normalizeContent = MyHelper.FixAttributes(tempnormalizeContent);
                 XParseDocument doc = MyHelper.ParseXmlDocument(normalizeContent);
-                var ownershipSummaryTable = XPath.GetElement("/table[1]", doc);
-                var activePositionTable = XPath.GetElement("/table[2]", doc);
-                var newSoldoutTable = XPath.GetElement("/table[3]", doc);
-                var holdingDetail = XPath.GetElement("/table[4]", doc);
-            //    ParseTable(aggregate, resultNode, ticker, "");
+                string temp;
+                for (int i = 1; i <= 4; i++)
+                {
+                    startIndex = normalizeContent.IndexOfOccurence(startTablePattern, i);
+                    endIndex = normalizeContent.IndexOf(endTablePattern, startIndex);
+                    temp = normalizeContent.Substring(startIndex, endIndex - startIndex + endTablePattern.Length);
+                    doc = MyHelper.ParseXmlDocument(temp);
+                    var ownershipSummaryTable = XPath.GetElement("//table", doc);
+                    ParseTable(aggregate, ownershipSummaryTable);
+                }
+                //var activePositionTable = XPath.GetElement("//table[2]", doc);
+                //var newSoldoutTable = XPath.GetElement("//table[3]", doc);
+                //var holdingDetail = XPath.GetElement("//table[4]", doc);
+             
                 return aggregate;
             }
             return null;
+        }
+
+        private void ParseTable (HoldingDataPointAggregate holding, XParseElement table)
+        {
+
         }
     }
 }
