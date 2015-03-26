fd14e99f51201d223cae35464981a70474250145
adamshe <adamshe@yahoo.com>
2015-03-25 20:57:04 -0400

    test

---
+++ NB.Core.Web/Models/Context/MarketContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NB.Core.Web.Models
+{
+    public class MarketContext
+    {
+        public float FairValue { get; set; }
+
+        public float TenYearBond { get; set; }
+
+        public float SpyTenYearReturn { get; set; }
+
+        public Dictionary<string, SectorContext> IndustryDictionary { get; set; }
+    }
+}
---
+++ NB.Core.Web/Models/Context/SectorContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NB.Core.Web.Models
+{
+    public class SectorContext
+    {
+    }
+}
